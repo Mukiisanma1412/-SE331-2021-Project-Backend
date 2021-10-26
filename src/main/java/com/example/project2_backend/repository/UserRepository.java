6f1f0a8dd505bf5d1276e32cb9996db5d8514305
IMthitinat <IMthitinat@users.noreply.github.com>
2021-10-26 15:55:19 +0700

    edit2 user Repository

--- src/main/java/com/example/project2_backend/repository/UserRepository.java
+++ src/main/java/com/example/project2_backend/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.example.project2_backend.repository;
 
-public interface UserRepository {
+import com.example.project2_backend.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository  extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
