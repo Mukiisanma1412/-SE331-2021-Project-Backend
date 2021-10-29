98f6279f706f63cf858d6171dd547ee8097c9e47
IMthitinat <IMthitinat@users.noreply.github.com>
2021-10-30 00:48:50 +0700

    cannot run the code

--- src/main/java/com/example/project2_backend/util/LabMapper.java
+++ src/main/java/com/example/project2_backend/util/LabMapper.java
@@ -5,9 +5,11 @@ import com.example.project2_backend.entities.VaccineDTO;
 import com.example.project2_backend.entities.patient;
 import com.example.project2_backend.entities.patientDTO;
 import com.example.project2_backend.security.entity.User;
+import com.example.project2_backend.security.entity.UserAuthDTO;
 import com.example.project2_backend.security.entity.UserDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapping;
 
 
 import java.util.List;
@@ -26,6 +28,10 @@ public interface LabMapper {
     UserDTO getUserDTO(User user);
 
     //@Mapping(target = "authorities", expression = "java(organizer.getUser().getAuthorities().stream().map(auth -> auth.getName().name()).collect(Collectors.toList()))")
+
+    UserDTO getRegisterDto(User user);
+    @Mapping(target = "authorities", expression = "java(user.getAuthorities().stream().map(auth -> auth.getName().name()).collect(Collectors.toList()))")
+    UserAuthDTO getUserAuthDTO(User user);
 
 
 }
